2690ce3d92f4a1f5902f4a68fceb0855d40f4b68
zhuyizheng <zhuyizheng@gmail.com>
2023-10-27 14:55:58 +0800

    Read accuracy from log

--- read_acc_from_log.py
+++ read_acc_from_log.py
@@ -1,8 +1,14 @@
 FILENAME = 'a_strict.txt'
-
+OUTPUTFILENAME = 'a_strict_acc.txt'
 import re
 pattern = r'Accuracy/Eval: (\d+\.\d+)'
 with open(FILENAME, 'r') as file:
     file_contents = file.read()
 matches = re.findall(pattern, file_contents)
 print(','.join(matches))
+
+# Open the output file for writing
+with open(OUTPUTFILENAME, 'w') as output_file:
+    # Write each match to the output file on a separate line
+    for match in matches:
+        output_file.write(match + '\n')
