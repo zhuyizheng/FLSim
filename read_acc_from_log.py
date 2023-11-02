9a72645487a663090d56d1e7d381c0ef0674fbfa
zhuyizheng <zhuyizheng@gmail.com>
2023-11-02 15:40:27 +0800

    add prob_zkp

--- read_acc_from_log.py
+++ read_acc_from_log.py
@@ -1,23 +1,36 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="extract accuracy")
 
-parser.add_argument("input", type=str, help="input file name")
+parser.add_argument("input", type=str, help="input file path")
 args = parser.parse_args()
 
-FILENAME = args.input
-assert FILENAME.endswith('.txt'), "wrong input file format!"
-OUTPUTFILENAME = FILENAME[:-4] + '_acc.txt'
+path = args.input
+file_list = os.listdir(path)
 
-import re
-pattern = r'Accuracy/Eval: (\d+\.\d+)'
-with open(FILENAME, 'r') as file:
-    file_contents = file.read()
-matches = re.findall(pattern, file_contents)
-print(','.join(matches))
+def acc_name_converter(name):
+    return name[:-4] + '_acc.txt'
 
-# Open the output file for writing
-with open(OUTPUTFILENAME, 'w') as output_file:
-    # Write each match to the output file on a separate line
-    for match in matches:
-        output_file.write(match + '\n')
+for name in file_list:
+    if name[-4:] != '.txt':
+        continue
+    if name[-8:] == '_acc.txt':
+        continue
+
+    FILENAME = os.path.join(path, name)
+    # assert FILENAME.endswith('.txt'), "wrong input file format!"
+    OUTPUTFILENAME = os.path.join(path, acc_name_converter(name))
+
+    import re
+    pattern = r'Accuracy/Eval: (\d+\.\d+)'
+    with open(FILENAME, 'r') as file:
+        file_contents = file.read()
+    matches = re.findall(pattern, file_contents)
+    print(','.join(matches))
+
+    # Open the output file for writing
+    with open(OUTPUTFILENAME, 'w') as output_file:
+        # Write each match to the output file on a separate line
+        for match in matches:
+            output_file.write(match + '\n')
