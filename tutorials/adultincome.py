be5c85199f3c86af1faae6e931edbb3e0d5147d0
zhuyizheng <zhuyizheng@gmail.com>
2023-11-16 09:54:02 +0800

    Revert "mlp"
    This reverts commit 61bc3b7a7ffd5a67b50992ca197eac0acd804318.

--- tutorials/adultincome.py
+++ tutorials/adultincome.py
@@ -40,9 +40,10 @@ class MLP_Model(nn.Module):
         # No activation function on the output
         x = self.output_layer(x)
 
+        print("forward:", x)
+
         # if self.task == "classification":
-        # x = F.softmax(x, dim=1)
-        x = F.sigmoid(x)
+        x = F.softmax(x, dim=1)
 
         return x
 
@@ -236,7 +237,7 @@ print(f"\nClients in total: {data_provider.num_train_users()}")
 
 # 1. Define our model, a simple CNN.
 # model = SimpleConvNet(in_channels=1, num_classes=11)
-model = MLP_Model(n_layers=4, input_dim=X_train.shape[1], hidden_dim=47, output_dim=1)
+model = MLP_Model(n_layers=4, input_dim=X_train.shape[1], hidden_dim=47, output_dim=2)
 
 
 # 2. Choose where the model will be allocated.
