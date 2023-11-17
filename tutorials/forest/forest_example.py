d3eb07fa19fd2192ce19de5743b0f0f8ae814fc2
zhuyizheng <zhuyizheng@gmail.com>
2023-11-17 18:20:19 +0800

    adultincome

--- tutorials/forest/forest_example.py
+++ tutorials/forest/forest_example.py
@@ -137,25 +137,6 @@ cat_idxs = [ i for i, f in enumerate(features) if f in categorical_columns]
 cat_dims = [ categorical_dims[f] for i, f in enumerate(features) if f in categorical_columns]
 
 
-# # Network parameters
-
-# In[8]:
-
-
-clf = TabNetClassifier(
-    n_d=64, n_a=64, n_steps=5,
-    gamma=1.5, n_independent=2, n_shared=2,
-    cat_idxs=cat_idxs,
-    cat_dims=cat_dims,
-    cat_emb_dim=1,
-    lambda_sparse=1e-4, momentum=0.3, clip_value=2.,
-    optimizer_fn=torch.optim.Adam,
-    optimizer_params=dict(lr=2e-2),
-    scheduler_params = {"gamma": 0.95,
-                     "step_size": 20},
-    scheduler_fn=torch.optim.lr_scheduler.StepLR, epsilon=1e-15
-)
-
 
 # # Training
 
@@ -179,6 +160,45 @@ y_test = train[target].values[test_indices]
 
 # In[10]:
 
+import numpy as np
+import math
+import torch
+from flsim.utils.example_utils import SimpleConvNet
+from torch.utils.data import Dataset
+from flsim.data.data_sharder import SequentialSharder, PowerLawSharder
+from flsim.utils.example_utils import DataLoader, DataProvider
+
+class ForestDataset(Dataset):
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+        assert self.features.size(0) == self.labels.size(0)
+    def __len__(self):
+        return self.features.size(0)
+    def __getitem__(self, user_id):
+        return self.features[user_id], self.labels[user_id]
+
+train_dataset = ForestDataset(torch.from_numpy(X_train).float(), torch.from_numpy(y_train).view(-1, 1))
+valid_dataset = ForestDataset(torch.from_numpy(X_valid).float(), torch.from_numpy(y_valid).view(-1, 1))
+test_dataset = ForestDataset(torch.from_numpy(X_test).float(), torch.from_numpy(y_test).view(-1, 1))
+
+NUM_CLIENTS = 1
+# 2. Create a sharder, which maps samples in the training data to clients.
+sharder = SequentialSharder(examples_per_shard=math.ceil(train_dataset.__len__() / NUM_CLIENTS))
+# sharder = PowerLawSharder(num_shards=NUM_CLIENTS, alpha=0.5)
+
+LOCAL_BATCH_SIZE = 16384
+
+# 3. Shard and batchify training, eval, and test data.
+fl_data_loader = DataLoader(
+    train_dataset=train_dataset,
+    eval_dataset=valid_dataset,
+    test_dataset=test_dataset,
+    sharder=sharder,
+    batch_size=LOCAL_BATCH_SIZE,
+    drop_last=False,
+)
+
 
 max_epochs = 100 if not os.getenv("CI", False) else 2
 
@@ -189,178 +209,145 @@ max_epochs = 100 if not os.getenv("CI", False) else 2
 from pytorch_tabnet.augmentations import ClassificationSMOTE
 aug = ClassificationSMOTE(p=0.2)
 
-clf.fit(
-    X_train=X_train, y_train=y_train,
-    eval_set=[(X_train, y_train), (X_valid, y_valid)],
-    eval_name=['train', 'valid'],
-    max_epochs=max_epochs, patience=100,
-    batch_size=16384, virtual_batch_size=256,
-    augmentations=aug
-) 
-
-
-# In[ ]:
-
-
-# plot losses
-plt.plot(clf.history['loss'])
-
-
-# In[ ]:
-
-
-# plot accuracy
-plt.plot(clf.history['train_accuracy'])
-plt.plot(clf.history['valid_accuracy'])
-
-
-# ### Predictions
-# 
-
-# In[ ]:
-
-
-# To get final results you may need to use a mapping for classes 
-# as you are allowed to use targets like ["yes", "no", "maybe", "I don't know"]
-
-preds_mapper = { idx : class_name for idx, class_name in enumerate(clf.classes_)}
-
-preds = clf.predict_proba(X_test)
-
-y_pred = np.vectorize(preds_mapper.get)(np.argmax(preds, axis=1))
-
-test_acc = accuracy_score(y_pred=y_pred, y_true=y_test)
-
-print(f"BEST VALID SCORE FOR {dataset_name} : {clf.best_cost}")
-print(f"FINAL TEST SCORE FOR {dataset_name} : {test_acc}")
-
-
-# In[ ]:
-
-
-# or you can simply use the predict method
-
-y_pred = clf.predict(X_test)
-test_acc = accuracy_score(y_pred=y_pred, y_true=y_test)
-print(f"FINAL TEST SCORE FOR {dataset_name} : {test_acc}")
-
-
-# # Save and load Model
-
-# In[ ]:
-
-
-# save state dict
-saved_filename = clf.save_model('test_model')
-
-
-# In[ ]:
-
-
-# define new model and load save parameters
-loaded_clf = TabNetClassifier()
-loaded_clf.load_model(saved_filename)
-
-
-# In[ ]:
-
-
-loaded_preds = loaded_clf.predict_proba(X_test)
-loaded_y_pred = np.vectorize(preds_mapper.get)(np.argmax(loaded_preds, axis=1))
-
-loaded_test_acc = accuracy_score(y_pred=loaded_y_pred, y_true=y_test)
-
-print(f"FINAL TEST SCORE FOR {dataset_name} : {loaded_test_acc}")
-
-
-# In[ ]:
-
-
-assert(test_acc == loaded_test_acc)
-
-
-# # Global explainability : feat importance summing to 1
-
-# In[ ]:
-
-
-clf.feature_importances_
-
-
-# # Local explainability and masks
-
-# In[ ]:
-
-
-explain_matrix, masks = clf.explain(X_test)
-
-
-# In[ ]:
-
-
-fig, axs = plt.subplots(1, 5, figsize=(20,20))
-
-for i in range(5):
-    axs[i].imshow(masks[i][:50])
-    axs[i].set_title(f"mask {i}")
-
-
-# # XGB
-
-# In[ ]:
-
-
-n_estimators = 1000 if not os.getenv("CI", False) else 20
-
-
-# In[ ]:
-
-
-from xgboost import XGBClassifier
-
-clf_xgb = XGBClassifier(max_depth=8,
-    learning_rate=0.1,
-    n_estimators=n_estimators,
-    verbosity=0,
-    silent=None,
-    objective="multi:softmax",
-    booster='gbtree',
-    n_jobs=-1,
-    nthread=None,
-    gamma=0,
-    min_child_weight=1,
-    max_delta_step=0,
-    subsample=0.7,
-    colsample_bytree=1,
-    colsample_bylevel=1,
-    colsample_bynode=1,
-    reg_alpha=0,
-    reg_lambda=1,
-    scale_pos_weight=1,
-    base_score=0.5,
-    random_state=0,
-    seed=None,)
-
-clf_xgb.fit(X_train, y_train,
-            eval_set=[(X_valid, y_valid)],
-            early_stopping_rounds=40,
-            verbose=10)
-
-
-# In[ ]:
-
-
-preds_valid = np.array(clf_xgb.predict_proba(X_valid, ))
-valid_acc = accuracy_score(y_pred=np.argmax(preds_valid, axis=1) + 1, y_true=y_valid)
-print(valid_acc)
-
-preds_test = np.array(clf_xgb.predict_proba(X_test))
-test_acc = accuracy_score(y_pred=np.argmax(preds_test, axis=1) + 1, y_true=y_test)
-print(test_acc)
-
-
-# In[ ]:
-
-
-
-
+data_provider = DataProvider(fl_data_loader)
+print(f"\nClients in total: {data_provider.num_train_users()}")
+
+clf = TabNetClassifier(
+    n_d=64, n_a=64, n_steps=5,
+    gamma=1.5, n_independent=2, n_shared=2,
+    cat_idxs=cat_idxs,
+    cat_dims=cat_dims,
+    cat_emb_dim=1,
+    lambda_sparse=1e-4, momentum=0.3, clip_value=2.,
+    optimizer_fn=torch.optim.Adam,
+    optimizer_params=dict(lr=2e-2),
+    scheduler_params = {"gamma": 0.95,
+                     "step_size": 20},
+    scheduler_fn=torch.optim.lr_scheduler.StepLR, epsilon=1e-15
+)
+
+
+clf._set_network()
+model = clf.network
+
+# nn.Module is clf.network
+# model = clf.network
+
+
+# 2. Choose where the model will be allocated.
+cuda_enabled = torch.cuda.is_available()
+device = torch.device(f"cuda:{0}" if cuda_enabled else "cpu")
+
+model, device
+
+from flsim.utils.example_utils import FLModel
+
+# 3. Wrap the model with FLModel.
+global_model = FLModel(model, device)
+assert(global_model.fl_get_module() == model)
+
+# 4. Move the model to GPU and enable CUDA if desired.
+if cuda_enabled:
+    global_model.fl_cuda()
+
+from flsim.interfaces.metrics_reporter import Channel
+from flsim.utils.example_utils import MetricsReporter
+
+# Create a metric reporter.
+metrics_reporter = MetricsReporter([Channel.TENSORBOARD, Channel.STDOUT])
+
+import inspect
+
+if VERBOSE:
+    print(inspect.getsource(MetricsReporter.compute_scores))
+    print(inspect.getsource(MetricsReporter.create_eval_metrics))
+    print(inspect.getsource(MetricsReporter.compare_metrics))
+
+import flsim.configs
+from flsim.utils.config_utils import fl_config_from_json
+from omegaconf import OmegaConf
+
+json_config = {
+    "trainer": {
+        "_base_": "base_sync_trainer",
+        # there are different types of aggregator
+        # fed avg doesn't require lr, while others such as fed_avg_with_lr or fed_adam do
+        "_base_": "base_sync_trainer",
+        "server": {
+            "_base_": "base_sync_server",
+            "server_optimizer": {
+                "_base_": "base_fed_avg_with_lr",
+                # "lr": args.lr,
+                "lr": 1.0,
+                "momentum": 0.9
+            },
+            # type of user selection sampling
+            "active_user_selector": {"_base_": "base_uniformly_random_active_user_selector"},
+        },
+        "client": {
+            # number of client's local epoch
+            "epochs": 1,
+            "optimizer": {
+                "_base_": "base_optimizer_sgd",
+                # client's local learning rate
+                # "lr": args.local_lr,
+                "lr": 0.02,
+                # client's local momentum
+                "momentum": 0,
+            },
+        },
+        # number of users per round for aggregation
+        "users_per_round": 1,
+        # "users_per_round": NUM_CLIENTS,
+        # total number of global epochs
+        # total #rounds = ceil(total_users / users_per_round) * epochs
+        # "epochs": args.epochs,
+        "epochs": 100,
+        # frequency of reporting train metrics
+        "train_metrics_reported_per_epoch": 100,
+        # frequency of evaluation per epoch
+        "eval_epoch_frequency": 1,
+        "do_eval": True,
+        # should we report train metrics after global aggregation
+        "report_train_metrics_after_aggregation": True,
+    }
+}
+cfg = fl_config_from_json(json_config)
+
+from hydra.utils import instantiate
+
+# Instantiate the trainer.
+trainer = instantiate(cfg.trainer, model=global_model, cuda_enabled=cuda_enabled)
+
+# Launch FL training.
+final_model, eval_score = trainer.train(
+    data_provider=data_provider,
+    metrics_reporter=metrics_reporter,
+    num_total_users=data_provider.num_train_users(),
+    distributed_world_size=1,
+    # malicious_count=MAX_MALICIOUS_CLIENTS,
+    malicious_count=0,
+    # attack_type=args.attack,  # 'scale', 'noise', 'flip'
+    attack_type='noise',
+    attack_param={},
+    # attack_param={'scale_factor': args.scale_factor,
+    #               'noise_std': args.noise_std,
+    #               'label_1': args.label_1,
+    #               'label_2': args.label_2},
+    # check_type=args.check,  # 'no_check', 'strict', 'prob_zkp'
+    check_type='no_check',
+    check_param={},
+    # check_param={'pred': args.pred, # 'l2norm', 'sphere', 'cosine'
+    #              'norm_bound': args.norm_bound},
+)
+
+
+# clf.fit(
+#     X_train=X_train, y_train=y_train,
+#     eval_set=[(X_train, y_train), (X_valid, y_valid)],
+#     eval_name=['train', 'valid'],
+#     max_epochs=max_epochs, patience=100,
+#     batch_size=16384, virtual_batch_size=256,
+#     augmentations=aug
+# )
+#
