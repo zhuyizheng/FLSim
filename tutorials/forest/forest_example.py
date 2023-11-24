005b9229c860e16e8ed9b34298e295d94a17a5b6
zhuyizheng <zhuyizheng@gmail.com>
2023-11-24 09:25:38 +0800

    tabnet

--- tutorials/forest/forest_example.py
+++ tutorials/forest/forest_example.py
@@ -22,7 +22,7 @@ parser.add_argument("--label-2", type=int, help="the label to change to if attac
 
 parser.add_argument("--check", type=str, help="check type: 'no_check', 'strict', 'prob_zkp'", default="no_check")
 parser.add_argument("--pred", type=str, help="check predicate: 'l2norm', 'sphere', 'cosine'", default="l2norm")
-parser.add_argument("--norm-bound-nn", type=float, help="nn l2 norm bound of l2norm check or cosine check", default=50.0)
+parser.add_argument("--norm-bound-nn", type=float, help="nn l2 norm bound of l2norm check or cosine check", default=30.0)
 parser.add_argument("--norm-bound-running-mean", type=float, help="running mean l2 norm bound of l2norm check or cosine check", default=100000000)
 parser.add_argument("--norm-bound-running-var", type=float, help="running var l2 norm bound of l2norm check or cosine check", default=100000000)
 
