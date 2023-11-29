306e27db09837d2d4d12a4de204e00a3d63cb1c9
zhuyizheng <zhuyizheng@gmail.com>
2023-11-29 10:15:47 +0800

    organa retract

--- tutorials/organamnist.py
+++ tutorials/organamnist.py
@@ -34,6 +34,8 @@ parser.add_argument("--local-batch-size", type=int, help="local batch size", def
 parser.add_argument("--local-epochs", type=int, help="number of epochs", default=1)
 parser.add_argument("--epochs", type=int, help="number of epochs", default=100)
 
+parser.add_argument("--gpu", type=int, help="gpu number", default=0)
+
 # Parse the command line arguments
 args = parser.parse_args()
 
@@ -124,7 +126,7 @@ model = SimpleConvNet(in_channels=1, num_classes=11)
 
 # 2. Choose where the model will be allocated.
 cuda_enabled = torch.cuda.is_available() and USE_CUDA
-device = torch.device(f"cuda:{0}" if cuda_enabled else "cpu")
+device = torch.device(f"cuda:{args.gpu}" if cuda_enabled else "cpu")
 
 model, device
 
